0a4ea1579edf6b1ba857e192485d633c5b226ce6
Omar Erminy <oerminy@novomind.com>
2017-01-12 10:46:18 +0100

    Initial commit

---
+++ src/main/java/omar/test/buildnumber_plugin_test/App.java
@@ -0,0 +1,13 @@
+package omar.test.buildnumber_plugin_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
